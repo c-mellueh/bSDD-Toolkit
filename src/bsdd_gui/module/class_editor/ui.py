4a18275a632ddaf5416af07b8adb125aae2b86b9
c-mellueh <christoph@mellueh.de>
2025-08-22 18:32:23 +0200

    add class editor ui

--- src/bsdd_gui/module/class_editor/ui.py
+++ src/bsdd_gui/module/class_editor/ui.py
@@ -1,1 +1,22 @@
+from PySide6.QtWidgets import QApplication, QWidget
 
+from bsdd_gui.resources.icons import get_icon
+from . import trigger
+from .qt import ui_ClassEditor
+
+
+class ClassEditor(QWidget, ui_ClassEditor.Ui_ClassEditor):
+    def __init__(
+        self,
+    ):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowIcon(get_icon())
+
+    # Open / Close windows
+    def closeEvent(self, event):
+        pass
+        # result = trigger.close_event(event)
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
