ca21888b51d92fcc0ede939d204f6b3351101227
c-mellueh <christoph@mellueh.de>
2025-08-28 11:28:13 +0200

    improve ui

--- src/bsdd_gui/module/property_editor/ui.py
+++ src/bsdd_gui/module/property_editor/ui.py
@@ -1,1 +1,50 @@
+from __future__ import annotations
 
+from PySide6.QtCore import (
+    QAbstractTableModel,
+    QSortFilterProxyModel,
+    QModelIndex,
+    Qt,
+    Signal,
+)
+from PySide6.QtWidgets import QWidget, QWidget, QDialog, QDialogButtonBox, QVBoxLayout
+from PySide6.QtGui import QStandardItemModel, QStandardItem, QPalette, QIcon
+
+from bsdd_gui.resources.icons import get_icon, get_link_icon
+from bsdd_parser import BsddClassProperty
+from . import trigger
+from bsdd_gui import tool
+from .qt.ui_Window import Ui_PropertyWindow
+
+
+class PropertyCreator(QDialog):
+    def __init__(self, bsdd_class_property: BsddClassProperty, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bsdd_class_property = bsdd_class_property
+        self.setWindowIcon(get_icon())
+        self.button_box = QDialogButtonBox(Qt.Horizontal)
+        # Layout
+        self._layout = QVBoxLayout(self)
+        self._layout.addWidget(self.button_box)
+        self.setLayout(self._layout)
+        self._editor_widget: PropertyEditor = None
+        self.new_button = self.button_box.addButton("Create", QDialogButtonBox.ActionRole)
+
+
+class PropertyEditor(QWidget, Ui_PropertyWindow):
+    closed = Signal()
+
+    def __init__(self, bsdd_class_property: BsddClassProperty, *args, mode="edit", **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setWindowIcon(get_icon())
+        self.setupUi(self)
+        self.mode = mode  # edit or new
+        self.bsdd_class_property = bsdd_class_property
+
+    def enterEvent(self, event):
+        trigger.update_window(self)
+        return super().enterEvent(event)
+
+    def closeEvent(self, event):
+        self.closed.emit()
+        return super().closeEvent(event)
