0241692d5434f8efe06097bf4468db6d8fa94e99
c-mellueh <christoph@mellueh.de>
2025-09-04 00:32:01 +0200

    matchup version numbers

--- src/bsdd_gui/__init__.py
+++ src/bsdd_gui/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__version__ = "0.1.0"  # needs to be on top of bsdd_gui import statements else ImportError
+__version__ = "0.1.1"  # needs to be on top of bsdd_gui import statements else ImportError
 
 import logging
 from typing import TYPE_CHECKING
